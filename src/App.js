8a2183e77a353a280a97a6fddcaf2cbf0f54a3c9
faizanhameed103 <fhameed103@gmail.com>
2023-11-11 20:30:02 +0500

    scroll functionality added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
-
+import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Testimonial from './components/Testimonial';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 function App() {
   return (
-    <>
-
-    </>
+    <div className='App'>
+      <Home />
+      <About />
+      <Testimonial/>
+      <Contact/>
+      <Footer/>
+    </div>
   );
 }
 
